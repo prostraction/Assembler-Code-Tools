29cf0e73becd23ae49c70cce9924f8ec819d6a89
prostraction <secret.darklight@github.com>
2020-11-27 16:38:34 +0300

    Refactored, write() reworked

--- Macroprocessor/main.c
+++ Macroprocessor/main.c
@@ -195,9 +195,9 @@ int read(unsigned char* name) {
 
 	while (fgets(buffer, 256, fp)) {
 		unsigned short count_of_args = parse_string(buffer, label, operator_asm, operand, comment);
+		// встетилось макроопределение
 		if (!strcmp(operator_asm, "MACRO") || !strcmp(operator_asm, "macro")) {
 			int count = 0;
-			printf("%__________ %d: \n", count_of_args);
 			unsigned short key = key_combine(label);
 			unsigned int hash_table_element_number = hash(key);
 			// обработка коллизии
@@ -206,7 +206,6 @@ int read(unsigned char* name) {
 
 				// коллизия встретилась впервые
 				if (macro[hash_table_element_number].begin_hash_table == NULL) {
-					/////////////////////////// *
 					macro[hash_table_element_number].begin_hash_table = malloc(sizeof(struct hash_table));
 					macro[hash_table_element_number].begin_hash_table->begin_code = NULL;
 					macro[hash_table_element_number].begin_hash_table->begin_hash_table = NULL;
@@ -229,7 +228,6 @@ int read(unsigned char* name) {
 					current->next->count_of_arugments = 0;
 					current->next->formal_operands = NULL;
 					current->next->key = 0;
-					//current->next->name = NULL;
 					memset(current->next->name, 0, 15);
 					current = current->next;
 					current->next = NULL;
@@ -238,19 +236,16 @@ int read(unsigned char* name) {
 			else {
 				current = &macro[hash_table_element_number];
 			}
-			////////////////////////////////////////////////////////////////////////////////////
+			// сохранение имени и параметров макроопределения
 			strcpy_s(current->name, strlen(label) + 1, label);
 			current->key = key;
 			unsigned int size_of_operand;
-			/////////////////////////////////////////////////////////////////////////////////////
 			current->formal_operands = NULL;
 			current->formal_operands = (char**)realloc(current->formal_operands, sizeof(char*) * (count_of_args + 1));
-			///////////////////////////////////////// ??????????????
 			for (int i = 0; i < count_of_args + 1; i++) {
 				current->formal_operands[i] = NULL;
 			}
 			current->count_of_arugments = 0;
-
 
 			for (unsigned int prev_size = 0, i = 0; i < strlen(operand); i++) {
 				if (operand[i] == ',' || (i == strlen(operand) - 1)) {
@@ -263,12 +258,9 @@ int read(unsigned char* name) {
 					temp_word[size_of_operand] = '\0';
 					prev_size = i + 2;
 					current->formal_operands[count] = temp_word;
-					printf("%s nnnnnnnnnnnnnnnnnnnnnnn\n", current->formal_operands[count]);
 					count++;
 					current->count_of_arugments++;
-					//free(temp_word);
 					temp_word = NULL;
-
 				}
 			}
 			memset(label, 0, sizeof(label));
@@ -276,25 +268,23 @@ int read(unsigned char* name) {
 			memset(operand, 0, sizeof(operand));
 			memset(comment, 0, sizeof(comment));
 
+			// сохранение кода макроопределения
 			struct list_code* current_code = NULL;
 			while (fgets(buffer, 256, fp)) {
 				parse_string(buffer, label, operator_asm, operand, comment);
 				if (!strcmp(operator_asm, "MEND") || !strcmp(operator_asm, "mend")) {
 					break;
 				}
-				///////////////////////////////////
 				if (current->begin_code == NULL) {
 					current->begin_code = malloc(sizeof(struct list_code));
 					current->begin_code->next = NULL;
 					current_code = current->begin_code;
-					//current_code->next = NULL;
 				}
 				else {
 					current_code = current->begin_code;
 					while (current_code->next != NULL) {
 						current_code = current_code->next;
 					}
-					////////////////// *
 					current_code->next = malloc(sizeof(struct list_code));
 					current_code = current_code->next;
 					current_code->next = NULL;
@@ -302,47 +292,60 @@ int read(unsigned char* name) {
 				current_code->label = NULL;
 				current_code->operand = NULL;
 				current_code->operator_asm = NULL;
-				if (strlen(label) > 1) {
-					current_code->label = malloc(strlen(label));
-				}
+
 				current_code->operator_asm = malloc(strlen(operator_asm));
-				
-				if (strlen(label) > 1) {
-					memset(current_code->label, '0', strlen(label));
-				}
 				memset(current_code->operator_asm, '0', strlen(operator_asm));
-				
-				if (strlen(label) > 1) {
+				strcpy_s(current_code->operator_asm, strlen(operator_asm) + 1, operator_asm);
+
+				if (strlen(label) > 0) {
+					current_code->label = malloc(strlen(label));
+					memset(current_code->label, '0', strlen(label));
 					strcpy_s(current_code->label, strlen(label) + 1, label);
 				}
-				strcpy_s(current_code->operator_asm, strlen(operator_asm) + 1, operator_asm);
-				//strcpy_s(current_code->operand, strlen(operand) + 1, operand);
 
-				//current_code->next = NULL;
+				// сохранение формального параметра в виде $n
 				bool operand_should_be_replaced = false;
 				for (int i = 0; i < current->count_of_arugments; i++) {
 					if (strstr(operand, current->formal_operands[i]) != NULL) {
-						printf("triggered\n");
 						char* temp_str = (char*)malloc(sizeof(char*) * 10);
 						sprintf(temp_str, "$%d\0", i);
-						current_code->operand = temp_str;
+						char* new_operand = malloc(sizeof(char) * strlen(operand + 1));
+						memset(new_operand, 0, strlen(operand + 1));
+						// замена формального параметра на запись $n
+						int j, z;
+						for (j = 0, z = 0; j < strlen(operand); j++, z++) {
+							if (operand[z] != '&') {
+								new_operand[z] = operand[j];
+							}
+							else {
+								new_operand[z++] = '$';
+								int k = 1;
+								if (j == 0)
+									j++;
+								while (temp_str[k] >= '0' && temp_str[k] <= '9') {
+									new_operand[z] = temp_str[k];
+									j++; k++;
+								}
+								j += sizeof(current->formal_operands[i]);
+							}
+						}
+						new_operand[z++] = '\0';
+						current_code->operand = new_operand;
 						operand_should_be_replaced = true;
+						free(temp_str);
 						break;
 					}
 				}
 				if (!operand_should_be_replaced) {
-					//current->begin_code->operand = malloc(strlen(operand));
-					//memset(current->begin_code->operand, '0', strlen(operand));
+
 					current_code->operand = malloc(strlen(operand));
 					strcpy_s(current_code->operand, strlen(operand) + 1, operand);
 				}
-				printf("^^^^^^^^^^^^^^^^^^^^^^ %s %s\n", current_code->operand, current_code->operator_asm);
 				memset(label, 0, sizeof(label));
 				memset(operator_asm, 0, sizeof(operator_asm));
 				memset(operand, 0, sizeof(operand));
 				memset(comment, 0, sizeof(comment));
 			}
-			////////////////////////////////////////////////////////////////////////////////////
 		}
 	}
 	return 1;
@@ -377,7 +380,7 @@ void write(char* name_input, char* name_output) {
 
 	while (fgets(buffer, 256, fp_in)) {
 		count_of_args = parse_string(buffer, label, operator_asm, operand, comment);
-		// skipping macro definition
+		// пропуск макроопределения
 		if (!strcmp(operator_asm, "MACRO") || !strcmp(operator_asm, "macro")) {
 			memset(buffer, 0, sizeof(buffer));
 			memset(label, 0, sizeof(label));
@@ -396,10 +399,8 @@ void write(char* name_input, char* name_output) {
 				}
 			}
 		}
-		// parsing other:
-		//
+		// обработка остального
 		if (strlen(buffer) > 1) {
-			printf("333333333333333333333 %d\n", count_of_args);
 			unsigned short key = key_combine(operator_asm);
 			unsigned int hash_table_element_number = hash(key_combine(operator_asm));
 			bool found = false;
@@ -422,62 +423,94 @@ void write(char* name_input, char* name_output) {
 						current_hash = current_hash->next;
 					}
 				}
-					if (found) {
-						fprintf(fp_out, "; ");
-						fprintf(fp_out, label);
-						fprintf(fp_out, ": ");
-						fprintf(fp_out, operator_asm);
-						fprintf(fp_out, " ");
-						fprintf(fp_out, operand);
-						fprintf(fp_out, "\n");
-
-						////////////////// 
-						// TO DO OPERAND
-						/////////////////////////////
-
-
-						struct list_code* current_code = current_hash->begin_code;
-						while (current_code != NULL) {
-
-							if (current_code->label != NULL) {
-								fprintf(fp_out, current_code->label);
-								fprintf(fp_out, ": ");
-							}
-							unsigned int index = -1;
-							char* to_be_replaced = malloc(sizeof(char) * strlen(current_code->operand));
-							memset(to_be_replaced, 0, strlen(current_code->operand));
-							for (int i = 0, j = 0, k = 1; i < strlen(current_code->operand); i++) {
-								if (current_code->operand[i] == '$') {
-									index = 0;
-									i++;
-									while (current_code->operand[i] >= '0' && current_code->operand[i] <= '9') {
-										to_be_replaced[j] = current_code->operand[i];
-										index += (current_code->operand[i] - '0') * k;
-										i++; j++; k += 10;
-									}
-									to_be_replaced[j] = '\0';
-								}
-							}
-							printf("2222222222222222222222222222222222		%d		%s\n", index, to_be_replaced);
-							fprintf(fp_out, current_code->operator_asm);
-							fprintf(fp_out, " ");
-							fprintf(fp_out, current_code->operand);
-							fprintf(fp_out, "\n");
-							current_code = current_code->next;
+				if (found) {
+					fprintf(fp_out, "; ");
+					fprintf(fp_out, label);
+					fprintf(fp_out, ": ");
+					fprintf(fp_out, operator_asm);
+					fprintf(fp_out, " ");
+					fprintf(fp_out, operand);
+					fprintf(fp_out, "\n");
+					fprintf(fp_out, label);
+					fprintf(fp_out, ": ");
+					char** fact_operands = NULL;
+					// объявить фактические параметры макроопределения
+					if (count_of_args > 0) {
+						fact_operands = (char**)realloc(fact_operands, sizeof(char*) * (count_of_args + 1));
+						for (int i = 0; i < count_of_args + 1; i++) {
+							fact_operands[i] = NULL;
 						}
-						free(current_code);
+						int size_of_operand = 0;
+						int count = 0;
+						// заполнить фактические параметры макроопределения
+						for (unsigned int prev_size = 0, i = 0; i < strlen(operand); i++) {
+							if (operand[i] == ',' || (i == strlen(operand) - 1)) {
+								size_of_operand = (i == strlen(operand) - 1) ? (i - prev_size + 1) : (i - prev_size);
+								char* temp_word = (char*)malloc(sizeof(char) * (size_of_operand + 1));
+								memset(temp_word, '0', size_of_operand + 1);
+								for (int k = 0, j = prev_size; k < size_of_operand; k++, j++) {
+									temp_word[k] = operand[j];
+								}
+								temp_word[size_of_operand] = '\0';
+								prev_size = i + 2;
+								fact_operands[count] = temp_word;
+								count++;
+								//free(temp_word);
+								temp_word = NULL;
+							}
+						}
 					}
-					if (!found) {
-						if (strlen(label) > 0) {
-							fprintf(fp_out, label);
+
+					// написать код макроопределения
+					struct list_code* current_code = current_hash->begin_code;
+					while (current_code != NULL) {
+						if (current_code->label != NULL) {
+							fprintf(fp_out, current_code->label);
 							fprintf(fp_out, ": ");
 						}
-						fprintf(fp_out, operator_asm);
+						fprintf(fp_out, current_code->operator_asm);
 						fprintf(fp_out, " ");
-						fprintf(fp_out, operand);
+						int index = -1;
+						// обработка фактических параметров
+						for (int i = 0, j = 0, k = 1; i < strlen(current_code->operand); i++) {
+							if (current_code->operand[i] == '$') {
+								index = 0;
+								i++;
+								// замена операндов
+								while (current_code->operand[i] >= '0' && current_code->operand[i] <= '9') {
+									index += (current_code->operand[i] - '0') * k;
+									i++; j++; k += 10;
+								}
+								if (fact_operands != NULL) {
+									fprintf(fp_out, fact_operands[index]);
+									i--;
+								}
+							}
+							else {
+								char* temp = malloc(sizeof(char) * 2);
+								temp[0] = current_code->operand[i];
+								temp[1] = '\0';
+								fprintf(fp_out, temp);
+								free(temp);
+							}
+						}
 						fprintf(fp_out, "\n");
+						current_code = current_code->next;
 					}
-					current_hash = NULL;
+					free(current_code);
+				}
+				// нет макроопределения
+				if (!found) {
+					if (strlen(label) > 0) {
+						fprintf(fp_out, label);
+						fprintf(fp_out, ": ");
+					}
+					fprintf(fp_out, operator_asm);
+					fprintf(fp_out, " ");
+					fprintf(fp_out, operand);
+					fprintf(fp_out, "\n");
+				}
+				current_hash = NULL;
 			}
 			else {
 				if (strlen(label) > 0) {
@@ -489,8 +522,6 @@ void write(char* name_input, char* name_output) {
 				fprintf(fp_out, operand);
 				fprintf(fp_out, "\n");
 			}
-			// иначе нет макроопределения
-			//printf("%s\n", operator_asm);
 		}
 		memset(buffer, 0, sizeof(buffer));
 		memset(label, 0, sizeof(label));
@@ -505,35 +536,42 @@ void debug_print() {
 	for (int i = 0; i < hash_table_size; i++) {
 		if (macro[i].key != 0) {
 
-			printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
+			printf("Arguments: (");
 			for (int j = 0; j < macro->count_of_arugments; j++) {
 				if (macro[i].formal_operands != NULL)
-					printf("%s \n", macro[i].formal_operands[j]);
+					if (j == macro->count_of_arugments - 1)
+						printf("%s", macro[i].formal_operands[j]);
+					else
+						printf("%s, ", macro[i].formal_operands[j]);
 			}
-			printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
-			printf("\ncode:\n");
+			printf(")\n");
+			printf("Code:\n");
 
 			struct list_code* current_code = macro[i].begin_code;
 			while (current_code != NULL) {
-				printf("+++++++++ %s %s\n", current_code->operator_asm, current_code->operand);
+				printf("--\t  %s %s\n", current_code->operator_asm, current_code->operand);
 				current_code = current_code->next;
 			}
+			printf("\n");
 
 			if (macro[i].begin_hash_table != NULL) {
-				printf("111111111111111111111111111111111111111111111111111111111111111111111 %p\n", macro[i].begin_hash_table);
 				struct hash_table* current_hash = macro[i].begin_hash_table;
 				while (current_hash != NULL) {
-					printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
+					printf("Arguments: (");
 					for (int j = 0; j < current_hash->count_of_arugments; j++) {
-						if (current_hash->formal_operands != NULL)
-							printf("%s \n", macro[i].formal_operands[j]);
+						if (current_hash->formal_operands != NULL) {
+							if (j == current_hash->count_of_arugments - 1)
+								printf("%s", macro[i].formal_operands[j]);
+							else
+								printf("%s, ", macro[i].formal_operands[j]);
+						}
 					}
-					printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
+					printf(")");
 
-					printf("\ncode:\n");
+					printf("\nCode:\n");
 					struct list_code* current_code = current_hash->begin_code;
 					while (current_code != NULL) {
-						printf("----------- %s %s\n", current_code->operator_asm, current_code->operand);
+						printf("--\t %s %s\n", current_code->operator_asm, current_code->operand);
 						current_code = current_code->next;
 					}
 					free(current_code);
